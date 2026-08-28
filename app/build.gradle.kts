658abf46d37e5575c5478dc94a2a57226d9cc97c
Grimforsaken <grimforsaken@gmail.com>
2026-08-28 08:14:29 -0500

    Bump app to interactive town shop build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.grimforsaken.dungeondicefrogs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-town-equipment"
+        versionCode = 2
+        versionName = "0.2.0-interactive-town-shops"
     }
 
     buildFeatures { compose = true }
