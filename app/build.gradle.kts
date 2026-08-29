00b5166b4a60bd9f9efa2521272bfebb3b95e79f
Grimforsaken <grimforsaken@gmail.com>
2026-08-29 13:46:36 -0500

    Bump version for startup crash fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.grimforsaken.dungeondicefrogs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0-persistent-procedural-dungeon"
+        versionCode = 4
+        versionName = "0.3.1-startup-fix-icons"
     }
 
     buildFeatures { compose = true }
